a451d40cec657c98168836bf7db1501d97daed30
Manan Jindal <manan.jindal@gmail.com>
2024-08-26 16:44:37 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import "./App.css";
+import { Button } from "@mananjindal/mjlib";
+
+function App() {
+  return (
+    <div className="App">
+      <Button />
+    </div>
+  );
+}
+
+export default App;
